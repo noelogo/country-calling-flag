48f7dda49292853c79a3b70e1ddbc9b697d82475
tokki <tokkiidea@gmail.com>
2014-08-07 17:01:39 +0800

    add resize flag

---
+++ flag/resize.py
@@ -0,0 +1,13 @@
+from glob import glob
+from os.path import splitext
+from PIL import Image
+imglist = glob("flag-img/*.gif")
+print imglist
+for imgpath in imglist:
+    img = Image.open(imgpath)
+    # here i use 44*28 size for the icon
+    img.thumbnail((44,28))
+    buildpath = imgpath.replace(".gif",".png")
+    buildpath = buildpath.replace("flag-img/","build-img/")
+    img.save(buildpath,"png")
+
