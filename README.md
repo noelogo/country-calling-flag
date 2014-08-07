57b911bb55ac685d8b097509e83c61db7ff7999b
tokki <tokkiidea@gmail.com>
2014-08-07 16:26:34 +0800

    i

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 country-calling-flag
 ====================
+
+this is a tool to build the json information you want
+
+check the build.py
+
+###run
+	$ python build.py
+
+### result
+**country.json ** for your project
+
+the default result is 
+
+	{
+	“call”:”86”,
+	”ch”:”\u4e2d\u56fd”,
+	“en”:”china”,
+	”ja”:”\u4e2d\u56fd”
+	}
+	
+you can change the build.py for your project
+
+the flag will be add soon
---
+++ build.py
@@ -0,0 +1,26 @@
+#-*-coding: utf-8 -*-
+import json
+f = open('country-source.txt', 'r')
+text = f.read()
+obj = json.loads(text)
+f.close()
+newlist = list()
+# you can build your json
+
+for e in obj:
+    for el in e["callingCode"]:
+        if el:
+            #print el
+            nset = {}
+            nset["en"]=e["name"]
+            nset["call"]= el
+            nset["ja"]=e["translations"]["ja"]
+            nset["ch"]=e["translations"]["ch"]
+            newlist.append(nset)
+        
+    #print e["translations"]["ja"]
+
+a = open('country.json','w')
+ttt = json.dumps(newlist)
+a.write(ttt)
+a.close()
